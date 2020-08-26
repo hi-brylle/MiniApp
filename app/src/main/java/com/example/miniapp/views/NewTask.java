e283927948f5c39618dc05bde34256da834aac96
Brylle <55132223+hi-brylle@users.noreply.github.com>
2020-08-27 00:17:31 +0800

    Add widget objects to Task class

--- app/src/main/java/com/example/miniapp/views/NewTask.java
+++ app/src/main/java/com/example/miniapp/views/NewTask.java
@@ -3,10 +3,19 @@ package com.example.miniapp.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.example.miniapp.R;
 
 public class NewTask extends AppCompatActivity {
+
+    private EditText editTextTask;
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private Button buttonSaveTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
