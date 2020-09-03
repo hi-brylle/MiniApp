db0069e9bc4e96c69f8d622fffbc89564055f6f4
Brylle <55132223+hi-brylle@users.noreply.github.com>
2020-09-03 11:32:23 +0800

    Make a read-all query method in DBManager that returns a JSONArray

--- app/src/main/java/com/example/miniapp/models/DBManager.java
+++ app/src/main/java/com/example/miniapp/models/DBManager.java
@@ -3,20 +3,34 @@ package com.example.miniapp.models;
 import android.util.Log;
 
 import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.DataSource;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.DatabaseConfiguration;
 import com.couchbase.lite.Document;
 import com.couchbase.lite.MutableDocument;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryBuilder;
+import com.couchbase.lite.Result;
+import com.couchbase.lite.ResultSet;
+import com.couchbase.lite.SelectResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class DBManager implements IDBManager {
-    private Database currentDatabase;   //open one database per session (yeah?)
-    private String DBToUseOrMake;       //DB name to use or make for current session
+    private Database currentDatabase;   // open one database per session (yeah?)
+    private String DBToUseOrMake;       // DB name to use or make for current session
 
     private DBManager(String dbName, DatabaseConfiguration config){
+        // DBToUseOrMake can either be a name for a shared database of login credentials
+        // or email names for user-specific data
         DBToUseOrMake = dbName;
 
+        // open database or create it if it doesn't exist
         try {
             currentDatabase = new Database(DBToUseOrMake, config);
         } catch (CouchbaseLiteException e) {
@@ -73,6 +87,33 @@ public class DBManager implements IDBManager {
 
     @Override
     public void create(HashMap<String, String> kvPairs) {
-        
+        // no need for custom ID cuz we're querying them all later anyway (?)
+        MutableDocument doc = new MutableDocument();
+
+        for(Map.Entry<String, String> kvPair : kvPairs.entrySet()){
+            // need to parse these values from String to either Date or Boolean later when getting them
+            doc.setValue(kvPair.getKey(), kvPair.getValue());
+        }
+
+        try {
+            currentDatabase.save(doc);
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public JSONArray getAll() {
+        Query query = QueryBuilder.select(SelectResult.all()).from(DataSource.database(currentDatabase));
+
+        try {
+            ResultSet results = query.execute();
+            return new JSONArray(results);
+        } catch (CouchbaseLiteException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
     }
 }
--- app/src/main/java/com/example/miniapp/models/IDBManager.java
+++ app/src/main/java/com/example/miniapp/models/IDBManager.java
@@ -1,7 +1,10 @@
 package com.example.miniapp.models;
 
+import org.json.JSONArray;
+
 import java.util.HashMap;
 
 public interface IDBManager {
     void create(HashMap<String, String> kvPairs);
+    JSONArray getAll();
 }
