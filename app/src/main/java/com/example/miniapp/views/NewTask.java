cc0b782038e020a8e0639cfd1c943070d2ee23fe
Brylle <55132223+hi-brylle@users.noreply.github.com>
2020-09-07 18:40:58 +0800

    Import default graphics

--- app/src/main/java/com/example/miniapp/views/NewTask.java
+++ app/src/main/java/com/example/miniapp/views/NewTask.java
@@ -1,10 +1,14 @@
 package com.example.miniapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.TimePickerDialog;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -50,6 +54,11 @@ public class NewTask extends AppCompatActivity implements DatePickerDialog.OnDat
     Date dateStart;
     Date dateCreated;
 
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager notificationManager;
+    private static final int NOTIFICATION_ID = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,6 +96,9 @@ public class NewTask extends AppCompatActivity implements DatePickerDialog.OnDat
                 validator.validate();
             }
         });
+
+        createNotificationChannel();
+        getNotificationBuilder();
     }
 
     @Override
@@ -99,6 +111,26 @@ public class NewTask extends AppCompatActivity implements DatePickerDialog.OnDat
     protected void onStop() {
         super.onStop();
         taskViewModel.closeDB();
+    }
+
+    public void createNotificationChannel(){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.YELLOW);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text");
+
+        return notifyBuilder;
     }
 
     @Override
@@ -194,7 +226,15 @@ public class NewTask extends AppCompatActivity implements DatePickerDialog.OnDat
 
         Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show();
         // TODO: Exit this Activity, go back to HomeScreen
+
+        sendNotification();
     }
+
+    private void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
 
     @Override
     public void update(Observable observable, Object o) {
