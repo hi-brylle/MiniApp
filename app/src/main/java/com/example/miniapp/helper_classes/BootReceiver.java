435906cc4cb19acfa591571ae6b585659de1d4dc
Brylle <55132223+hi-brylle@users.noreply.github.com>
2020-10-11 20:57:40 +0800

    Merge secure shared pref to master

--- app/src/main/java/com/example/miniapp/helper_classes/BootReceiver.java
+++ app/src/main/java/com/example/miniapp/helper_classes/BootReceiver.java
@@ -3,7 +3,6 @@ package com.example.miniapp.helper_classes;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import java.util.Objects;
 
