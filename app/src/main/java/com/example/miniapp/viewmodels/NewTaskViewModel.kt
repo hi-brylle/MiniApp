b20f8084968f785c1bba6957763e629197f4127e
Brylle <55132223+hi-brylle@users.noreply.github.com>
2020-10-16 15:02:48 +0800

    Convert NewTaskViewModel to kotlin

--- app/src/main/java/com/example/miniapp/viewmodels/NewTaskViewModel.kt
+++ app/src/main/java/com/example/miniapp/viewmodels/NewTaskViewModel.kt
@@ -1,62 +1,51 @@
-package com.example.miniapp.viewmodels;
+package com.example.miniapp.viewmodels
 
-import android.annotation.SuppressLint;
-import android.net.Uri;
+import android.annotation.SuppressLint
+import android.net.Uri
+import com.example.miniapp.helper_classes.log
+import com.example.miniapp.models.IUserDBManager
+import java.text.DateFormatSymbols
+import java.util.*
 
-import com.example.miniapp.helper_classes.Logger;
-import com.example.miniapp.models.IUserDBManager;
-
-import java.text.DateFormatSymbols;
-import java.util.Calendar;
-import java.util.Date;
-
-public class NewTaskViewModel {
-    private IUserDBManager dbManager;
-
-    public NewTaskViewModel(IUserDBManager dbManager){
-        this.dbManager = dbManager;
+class NewTaskViewModel(private val dbManager: IUserDBManager) {
+    fun openDB() {
+        dbManager.openDB()
     }
 
-    public static String  dateRepresentation(int year, int month, int day) {
-        // needs localization
-        String monthName = new DateFormatSymbols().getMonths()[month];
-
-        return day + " " + monthName + " " + year;
+    fun closeDB() {
+        dbManager.closeDB()
     }
 
-    @SuppressLint("DefaultLocale")
-    public static String timeRepresentation(int hr, int min) {
-        int hour = hr % 12;
-        if (hour == 0) hour = 12;
-
-        return String.format("%02d:%02d %s", hour, min, hr < 12 ? "am" : "pm");
-
+    fun submit(task: String, created: Date, start: Date, imageURI: Uri?, completeAddress: String?) {
+        val imageURIString = imageURI?.toString() ?: ""
+        val address = completeAddress ?: ""
+        dbManager.create(task, created, start, imageURIString, address)
+        log("Task: $task")
+        log("Created: $created")
+        log("Start: $start")
+        log("URI: $imageURIString")
+        log("Address: $address")
     }
 
-    public void openDB(){
-        dbManager.openDB();
+    fun isValid(dateTimeSelected: Date): Boolean {
+        val now = Calendar.getInstance().time
+        return dateTimeSelected.after(now)
     }
 
-    public void closeDB(){
-        dbManager.closeDB();
-    }
+    companion object {
+        @JvmStatic
+        fun dateRepresentation(year: Int, month: Int, day: Int): String {
+            // needs localization
+            val monthName = DateFormatSymbols().months[month]
+            return "$day $monthName $year"
+        }
 
-    public void submit(String task, Date created, Date start, Uri imageURI, String completeAddress){
-
-        String imageURIString = imageURI == null ? "" : imageURI.toString();
-        String address = completeAddress == null ? "" : completeAddress;
-
-        dbManager.create(task, created, start, imageURIString, address);
-
-        Logger.log("Task: " + task);
-        Logger.log("Created: " + created);
-        Logger.log("Start: " + start);
-        Logger.log("URI: " + imageURIString);
-        Logger.log("Address: " + address);
-    }
-
-    public boolean isValid(Date dateTimeSelected) {
-        Date now = Calendar.getInstance().getTime();
-        return dateTimeSelected.after(now);
+        @JvmStatic
+        @SuppressLint("DefaultLocale")
+        fun timeRepresentation(hr: Int, min: Int): String {
+            var hour = hr % 12
+            if (hour == 0) hour = 12
+            return String.format("%02d:%02d %s", hour, min, if (hr < 12) "am" else "pm")
+        }
     }
 }
